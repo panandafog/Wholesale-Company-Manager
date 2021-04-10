18f150b43df65944847a218010cbb729b64256ea
panandafog <panandafog@gmail.com>
2021-04-10 13:53:21 +0300

    Initial Commit

---
+++ Wholesale Company Manager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Wholesale Company Manager
+//
+//  Created by panandafog on 10.04.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
